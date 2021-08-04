b484f33593113daf03884c3f9e8676e57cbcf177
yasheshmodh <86482572+yasheshmodh@users.noreply.github.com>
2021-08-04 17:50:22 +0300

    Add files via upload

---
+++ Project_Template_24-main (4)/Project_Template_24-main/js/ComputerArcher.js
@@ -0,0 +1,35 @@
+class ComputerArcher {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: true
+      };
+  
+      this.body = Bodies.rectangle(x, y, width, height, options);
+  
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("./assets/computerArcher.png");
+  
+      World.add(world, this.body);
+      
+    }
+  
+   
+    display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      if (keyIsDown(DOWN_ARROW) && angle < -1.2) {
+        angle += 0.01; Matter.Body.setAngle(this.body, angle);
+        } if (keyIsDown(UP_ARROW) && angle > -1.9) {
+             angle -= 0.01; Matter.Body.setAngle(this.body, angle); 
+           }
+   
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
+  
